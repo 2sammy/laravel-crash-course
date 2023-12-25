997fe566e0ea8d3e2796f7bb2ee4db8205fbe8f5
2sammy <samuelmanoah61@gmail.com>
2023-12-25 15:20:09 +0300

    lesson 1-10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\select;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,63 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = User::where('id', 1) ->first();
+
+    //return view('welcome');
+    //fetching  all users
+    //$users = DB::select('select * from users where email=?', ["samuelmanoah61@gmail.com"]);
+  //  $users = DB::table('users') ->get();
+   // $users = DB::table('users') ->first();
+    //create new user
+    /*$user = DB::insert('insert into users(name, email, password) values(?,?,?)', [
+        "sanchez",
+        "sanchez01@gmail.com",
+        "password"
+
+
+    ]);
+    $user = User::create([
+        'name' => 'manoah',
+        'email' => 'manoahs02@gmail.com',
+        'password' => 'manoah'
+    ]);*/
+$user= User::create([
+    'name' => 'osengo',
+    'email' => 'osengo123@gmail.com',
+    'password'=> 'osengo',
+]);
+    /*$user =DB::table('users') ->insert([
+        'name' => 'sam',
+        'email' => 'sammuel012@gmail.com',
+        'password' => 'sammy'
+    ]);*/
+
+    //update user
+    //$user = DB::update("update users set email=? where id=?", [
+      //  'manoah61@gmail.com',
+        //3,
+    //]);
+
+    /*$user= DB::table('users')->where('id', 10) -> update([
+        'email' => 'samuel012@gmail.com'
+
+    ]);*/
+
+    //delete user
+//    $user = DB::delete("delete from users where id=2");
+
+/*$user =DB::table('users')-> where('id',10) ->delete();*/
+    dd($user);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
